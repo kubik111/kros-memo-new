05fa7d56e90aaa8d7398b445f4b4411cf52300ad
Jakub Svancár <jakub.svancar@kros.sk>
2020-10-06 10:05:40 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavContainerComponent } from './nav-container/nav-container.component';
+import { SearchComponent } from './nav-container/search/search.component';
+import { DetailComponent } from './detail/detail.component';
+import { DatePickerComponent } from './detail/date-picker/date-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavContainerComponent,
+    SearchComponent,
+    DetailComponent,
+    DatePickerComponent
   ],
   imports: [
     BrowserModule,
